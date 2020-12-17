9063dcd54e0fe2a09398979b9a1a05783482b739
Hekmat Hamidi <theonehamidi@gmail.com>
2020-12-16 21:20:11 -0500

    removed unwanted code

--- pages/api/schemas/index.js
+++ pages/api/schemas/index.js
@@ -1,15 +1,7 @@
 import { gql } from 'apollo-server-micro';
 
 export const typeDefs = gql`
-  type User {
-    id: ID
-    login: String
-    avatar_url: String
-  }
-
   type Query {
-    getUsers: [User]
-    getUser(name: String!): User!
     getPopularGames(limit: Int): String
     getUpcomingGames(limit: Int): String
     getNewGames(limit: Int): String
